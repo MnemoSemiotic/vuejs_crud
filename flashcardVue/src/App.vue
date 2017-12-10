c801affb28bb505a2ff98d05420f5605b3759dad
Andrew Roberts <clownfragment@gmail.com>
2017-12-10 16:22:08 -0700

    submit fields added

--- flashcardVue/src/App.vue
+++ flashcardVue/src/App.vue
@@ -2,6 +2,21 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http Requests</h1>
+        <div class="form-group">
+          <label>Question</label>
+          <input class="form-control" type="text">
+        </div>
+        <div class="form-group">
+          <label>Answer</label>
+          <input class="form-control" type="text">
+        </div>
+        <button class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+
   </div>
 
 </template>
